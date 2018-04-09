724c4af25147b2d135ed29085e15aeced5641182
Ravi Yadav <raviy8408@gmail.com>
2018-04-09 21:06:21 +0530

    Section 2 ml section Q2 test mode

--- test_sec2_q2.py
+++ test_sec2_q2.py
@@ -1,9 +1,23 @@
+"""
+Author:
+Date:
+TOpic:
+"""
+
+###########################################################################################3
+# COFIGURATION
+###########################################################################################3
+
 from pyspark.sql import SparkSession
 from pyspark.mllib.regression import *
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql.window import Window
 import sys
+from pyspark.ml.feature import VectorAssembler
+from pyspark.ml.regression import LinearRegression
+from pyspark.ml.regression import GBTRegressor
+from math import sqrt
 
 spark = SparkSession.builder \
     .master("local[*]") \
@@ -13,9 +27,15 @@ spark = SparkSession.builder \
 sc = spark.sparkContext
 
 sc.setLogLevel("ERROR")
+sc.setCheckpointDir('C://Users/Ravi/PycharmProjects/WeblogChallenge/checkpoint/')
+
+# print(sc)
 
 REPARTITION_FACTOR = int(sc._jsc.sc().getExecutorMemoryStatus().size()) * 10
-print(REPARTITION_FACTOR)
+# print(REPARTITION_FACTOR)
+
+# UTILS FUNCTIONS
+_minutesLambda = lambda i: i * 60
 
 ###########################################################################################3
 
@@ -23,10 +43,13 @@ print(REPARTITION_FACTOR)
 ###########################################################################################3
 # DATA INGESTION
 ###########################################################################################3
+
 raw_data = spark.read.option("delimiter", " ").csv("C://Users/Ravi/PycharmProjects/WeblogChallenge/data") \
-    .sample(False, 0.01, 42)
+    .sample(False, 0.001, 42)
 
 # print(raw_data.count())
+
+print("Ingesting Data...\n ")
 
 raw_data_w_cols = raw_data \
     .withColumnRenamed("_c0", "timestamp") \
@@ -46,17 +69,27 @@ raw_data_w_cols = raw_data \
     .withColumnRenamed("_c14", "ssl_protocol") \
     .repartition(REPARTITION_FACTOR)
 
-# raw_data_w_cols.show()
+# print(raw_data_w_cols.select(col("ssl_protocol")).distinct().count())
 
-##################################################################################
+raw_data_w_cols_clean = raw_data_w_cols \
+    .withColumn("request_processing_time_clean",
+                when(col("request_processing_time") == "-1", None).otherwise(col("request_processing_time"))) \
+    .withColumn("backend_processing_time_clean",
+                when(col("backend_processing_time") == "-1", None).otherwise(col("backend_processing_time"))) \
+    .withColumn("response_processing_time_clean",
+                when(col("response_processing_time") == "-1", None).otherwise(col("response_processing_time"))) \
+    .withColumn("elb_status_code_clean", concat_ws("_", lit("elb_status"), col("elb_status_code"))) \
+    .drop("elb_status_code") \
+    .withColumnRenamed("elb_status_code_clean", "elb_status_code") \
+    .withColumn("backend_status_code_clean", concat_ws("_", lit("backend_status"), col("backend_status_code"))) \
+    .drop("backend_status_code") \
+    .withColumnRenamed("backend_status_code_clean", "backend_status_code")
 
-###########################################################################################
-# SECTION 1 : Processing & Analytical goals
-###########################################################################################
+# print(raw_data_w_cols_clean.select(col("user_agent")).distinct().show())
 
-# Q1: Sessionize the web log by IP. Sessionize = aggregrate all page hits by visitor/IP during a fixed time window.
 
-_sessionized = raw_data_w_cols \
+print("Pre-processing Data...")
+_pre_proc = raw_data_w_cols_clean \
     .withColumn("IP", split(col("client"), ":").getItem(0)) \
     .withColumn("request_split", split(col("request"), " ")) \
     .withColumn("request_type", col("request_split").getItem(0)) \
@@ -64,6 +97,11 @@ _sessionized = raw_data_w_cols \
                 lower(split(split(col("request_split").getItem(1), "/").getItem(2), ":").getItem(0).cast(StringType()))) \
     .withColumn("http_version", col("request_split").getItem(2)) \
     .withColumn("unix_tmpstmp", to_utc_timestamp(col("timestamp"), "ISO 8601")) \
+    .withColumn("date", to_date(col("unix_tmpstmp"))) \
+    .withColumn("hour", hour(col("unix_tmpstmp"))) \
+    .withColumn("minute", minute(col("unix_tmpstmp"))) \
+    .withColumn("dummy_count", lit(1.0)) \
+    .replace(["\"GET"], ["GET"], "request_type") \
     .withColumn("lagged_tmpstmp", lag(col("unix_tmpstmp"), 1).over(Window.partitionBy("IP").orderBy("unix_tmpstmp"))) \
     .withColumn("new_session",
                 coalesce(unix_timestamp(col("unix_tmpstmp")) - unix_timestamp(col("lagged_tmpstmp")), lit(0.0)).cast(
@@ -74,4 +112,20 @@ _sessionized = raw_data_w_cols \
                                          col("sessionized_temp").cast(StringType()))) \
     .drop("new_session") \
     .drop("sessionized_temp") \
+    .drop("lagged_tmpstmp") \
     .repartition(REPARTITION_FACTOR)
+
+# print("_pre_proc schema:")
+# _pre_proc.printSchema()
+
+# creating feature set 1
+
+model_input_1 = _pre_proc \
+    .withColumn("IP_URL_visit", count("URL").over(
+    Window.partitionBy("IP").orderBy("unix_tmpstmp").rangeBetween(Window.unboundedPreceding,
+                                                                  Window.unboundedFollowing)))
+# .groupby("sessionized","IP_URL_visit")\
+# .agg(min(col("unix_tmpstmp")).alias("session_start_hour"))
+
+
+model_input_1.select(col("IP_URL_visit")).distinct().orderBy(["IP_URL_visit"], ascending=[0, 1]).show(5)
